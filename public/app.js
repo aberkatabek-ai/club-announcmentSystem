6f2faf5ce507bdd32c514e9b0f9c695ce5867ac1
Berk Atabek <aberkatabek@gmail.com>
2026-01-28 19:24:29 +0300

    Remove extra login button

--- public/app.js
+++ public/app.js
@@ -20,7 +20,6 @@ const loginInfoEl = document.getElementById("loginInfo");
 const whoamiEl = document.getElementById("whoami");
 const loginUserEl = document.getElementById("loginUser");
 const loginPassEl = document.getElementById("loginPass");
-const loginBtnEl = document.getElementById("loginBtn");
 const authTabLogin = document.getElementById("authTabLogin");
 const authTabLogout = document.getElementById("authTabLogout");
 const authToggleBtn = document.getElementById("authToggle");
@@ -135,7 +134,6 @@ function handleLoginEnter(e) {
 
 loginUserEl.addEventListener("keydown", handleLoginEnter);
 loginPassEl.addEventListener("keydown", handleLoginEnter);
-if (loginBtnEl) loginBtnEl.addEventListener("click", doLogin);
 
 async function doLogout() {
   await ensureCsrf();
